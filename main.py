7c5ccbafda3ce91f5f1dc5772a74f4b42e935557
Berend Vandenbussche <berend.vdb@outlook.com>
2022-05-22 13:26:20 +0200

    Added flask api

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 from temperature import temperature
+from flask import Flask, jsonify, request, url_for, json
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+endpoint = '/api/v1'
 temperature = temperature(27, '28-00000aee38a9')
-print('dt11 sensor: current temperature = {temp} and humidity {hum}'.format(temp = str(temperature.read_temperature_humidity_sensor().get('temperature')), hum = str(temperature.read_temperature_humidity_sensor().get('humidity'))), 'One wire sensor: ' + str(temperature.read_one_wire_temperature()))
+
+@app.route(endpoint + '/pool/temperature', methods=['GET'])
+def get_pool_temperature():
+    if (request.method == 'GET'):
+        return jsonify(temperature.read_one_wire_temperature())
+
+@app.route(endpoint + '/weather/temperature', methods=['GET'])
+def get_temperature():
+    if (request.method == 'GET'):
+        return jsonify(temperature.read_temperature_humidity_sensor().get('temperature'))
+
+@app.route(endpoint + '/weather/humidity', methods=['GET'])
+def get_humidity():
+    if (request.method == 'GET'):
+        return jsonify(temperature.read_temperature_humidity_sensor().get('humidity'))
+
+
+if __name__ == '__main__':
+    flask.run(app, host="0.0.0.0", port=5500)
