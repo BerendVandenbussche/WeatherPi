bee3e277e116151d948f34ee0d9c262863a0c798
Berend Vandenbussche <berend@inuits.eu>
2023-06-27 21:26:26 +0200

    Add get & set methods to db class

--- database.py
+++ database.py
@@ -3,3 +3,16 @@ import sqlite3
 class database:
     def __init__(self, database_file):
         self.database = sqlite3.connect(database_file)
+        self.cursor = database.cursor()
+        print('Succesfully connected to database with name {0}!'.format(database_file))
+
+    
+    def get_data(self, query):
+        data = self.cursor.execute(query)
+        return data.fetchall()
+
+
+    def set_data(self):
+        data = self.cursor.execute(query)
+        self.database.commit()
+        return data.fetchall()
