a31c15a6680c4982f1d23fd0648901ca05ba274b
Berend Vandenbussche <berend@inuits.eu>
2023-06-28 08:55:03 +0200

    Catch error connecting to db

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ import sqlite3
 
 class database:
     def __init__(self, database_file):
-        self.database = sqlite3.connect(database_file)
-        self.cursor = self.database.cursor()
-        print('Succesfully connected to database with name {0}!'.format(database_file))
+        try:
+            self.database = sqlite3.connect(database_file)
+            self.cursor = self.database.cursor()
+            print('Succesfully connected to database with name {0}!'.format(database_file))
+        except:
+            print('Could not connect to database with name {0}, does the file exist?'.format(database_file))
 
     
     def get_data(self, query):
